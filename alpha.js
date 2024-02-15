4b40fd2feab3c48e4f05e6f2e565d89ea18a4168
HyCute <hycute.ts@gmail.com>
2024-02-16 02:29:34 +0600

    2:29

--- alpha.js
+++ alpha.js
@@ -1,19 +1,19 @@
-function addClass(elementId){
+function addClass(elementId) {
     const id = document.getElementById(elementId);
     id.classList.add('hidden');
 }
 
-function removeClass(elementId){
+function removeClass(elementId) {
     const id = document.getElementById(elementId);
     id.classList.remove('hidden');
 }
 
 // random alphabet genarate 
-function randomAlphabet(){
+function randomAlphabet() {
     const randomValue = Math.random();
     const value = randomValue * 25;
     const index = Math.round(value);
-    
+
     const letter = 'abcdefghijklmnopqrstubwxyz/'
     const letterString = letter.split('');
     const alphabet = letterString[index];
@@ -21,32 +21,59 @@ function randomAlphabet(){
 }
 
 //set alphabet
-function setAlphabet(){
+function setAlphabet() {
     const alphabet = randomAlphabet();
     const letter = document.getElementById('letter');
     letter.innerText = alphabet;
+    addRemoveBg(alphabet, alphabet)
+    return letter;
 }
 
 // add or remove background of key 
-function addRemoveBg(elementId){
+function addRemoveBg(elementId, elementId2) {
     const id = document.getElementById(elementId);
     id.classList.add('bg-green-600');
-
-}
-
-function enterKey(){
-    const keyid = document.addEventListener('keyup', function (event){
-    const key = keyid.key;
-    return key;
-    })
+    const id2 = document.getElementById(elementId);
+    id2.classList.remove('bg-green-600');
 
 }
 
 
 
-function homeScreen(){
+eneterKey();
+function homeScreen() {
     addClass('home-screen');
     removeClass('game-screen');
     setAlphabet();
+
 }
-console.log(enterKey())
+
+// function eneterKey(event){
+//     if(event.key==='Enter'){
+//         homeScreen()
+//     }
+
+// }
+// document.addEventListener('keyup', eneterKey)
+
+function eneterKey() {
+    document.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            addClass('home-screen');
+            removeClass('game-screen');
+            return setAlphabet();
+        }
+    })
+}
+
+function playGame() {
+    const letter = document.getElementById('letter')
+    document.addEventListener('keyup', function (event) {
+        if (event.key === letter.innerText.toLowerCase()) {
+            setAlphabet()
+        }
+    })
+}
+
+playGame();
+
